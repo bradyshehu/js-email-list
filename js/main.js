68b345d2cc58c94040b317d7f37fc594ef72595c
bradyshehu <shehu.brady@gmail.com>
2025-04-23 16:07:51 +0200

    filled array in console

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-const emailArray = [];
 const apiMail = `https://flynn.boolean.careers/exercises/api/random/mail`;
+function getEmailArray() {
+  const emailArray = [];
+  console.log("PRIMA DEL FOR");
+  for (i = 0; i < 10; i++) {
+    axios.get(apiMail).then((response) => {
+      emailArray.push(response.data.response);
 
-for (i = 0; i < 10; i++) {
-  axios
-
-    .get(apiMail)
-    .then((response) => {
-      Promise.all(emailArray).push(response.data.response);
-    })
-    .then(() => {
-      console.table(emailArray);
+      if (emailArray.length === 10) {
+        console.table(emailArray);
+        emailArray.forEach((email) => {
+          // GENERA HTML
+        });
+      }
     });
+  }
 }
+getEmailArray();
